9744beb76d552b9e84993b041e9a8d1e2622b23f
Kas2002 <kasleenders@gmail.com>
2021-11-25 16:20:32 +0100

    hooder feater
    dank

---
+++ components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="nav">
+    <a href="#">Help</a>
+    <a href="#">onder</a>
+    <a href="#">aan</a>
+</div>
+</template>
+
+<script>
+export default {
+    name:'Footer'
+}
+</script>
+<style>
+.nav{
+    
+}
+</style>
